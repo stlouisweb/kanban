6f80098c7e11f974366bb1cf6f1dbedb7780e6bc
stlouisweb <stlouisweb@gmail.com>
2016-03-16 21:05:50 -0500

    editable notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -32,7 +32,7 @@ export default class App extends React.Component {
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-          <Notes notes={notes} />
+          <Notes notes={notes} onEdit={this.editNote} />
       </div>
     );
   }
@@ -44,5 +44,18 @@ export default class App extends React.Component {
         task: 'New task'
       }])
     });
+  };
+
+  editNote = (id, task) => {
+    if(!task.trim()) {
+      return;
+    }
+    const notes = this.state.notes.map(note => {
+      if(note.id === id && task) {
+        note.task = task;
+      }
+      return note;
+    });
+    this.setState({notes});
   };
 }
