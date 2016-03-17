961b2cfc6cf833bea37ac77376ac95c11637f126
stlouisweb <stlouisweb@gmail.com>
2016-03-16 21:11:20 -0500

    delete notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -32,10 +32,21 @@ export default class App extends React.Component {
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-          <Notes notes={notes} onEdit={this.editNote} />
+          <Notes notes={notes}
+            onEdit={this.editNote}
+            onDelete={this.deleteNote} />
+
       </div>
     );
   }
+
+  deleteNote = (id, e) => {
+    e.stopPropagation();
+
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== id)
+    });
+  };
 
   addNote = () => {
     this.setState({
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -25,7 +25,17 @@ export default class Note extends React.Component {
       onKeyPress={this.checkEnter} />;
   };
   renderNote = () => {
-    return <div onClick={this.edit}>{this.props.task}</div>;
+    const onDelete = this.props.onDelete;
+
+    return (
+      <div onClick={this.edit}>
+        <span>{this.props.task}</span>
+        {onDelete ? this.renderDelete() : null }
+      </div>
+    );
+  };
+  renderDelete = () => {
+    return <button onClick={this.props.onDelete}>x</button>;
   };
   edit = () => {
     this.setState({
