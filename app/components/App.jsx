58b0dc3dde0f35faeac7afe809b5cf6903512a99
stlouisweb <stlouisweb@gmail.com>
2016-03-17 06:48:54 -0500

    Getting started with Flux, adds NoteActions and NoteStore. Adds Alt lib.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,37 +1,30 @@
-import uuid from 'node-uuid';
 import React from 'react';
-
 import Notes from './Notes.jsx';
-
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn Webpack'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do laundry'
-        }
-      ]
-    };
+    this.state = NoteStore.getState();
   }
+
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged);
+  }
+  componentWillUnmount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+  storeChanged = (state) => {
+    this.setState(state);
+  };
 
   render() {
     const notes = this.state.notes;
     return (
       <div>
-        <button onClick={this.addNote}>+</button>
+        <button className="add-note" onClick={this.addNote}>+</button>
           <Notes notes={notes}
             onEdit={this.editNote}
             onDelete={this.deleteNote} />
@@ -40,33 +33,20 @@ export default class App extends React.Component {
     );
   }
 
-  deleteNote = (id, e) => {
+  deleteNote(id, e) {
     e.stopPropagation();
+    NoteActions.delete(id);
+  }
 
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
-    });
-  };
+  addNote() {
+    NoteActions.create({task: 'New task'});
+  }
 
-  addNote = () => {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
-    });
-  };
-
-  editNote = (id, task) => {
+  editNote(id, task) {
     if(!task.trim()) {
       return;
     }
-    const notes = this.state.notes.map(note => {
-      if(note.id === id && task) {
-        note.task = task;
-      }
-      return note;
-    });
-    this.setState({notes});
-  };
+    NoteActions.update({id, task});
+  }
+
 }
