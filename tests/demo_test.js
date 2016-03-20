d07ce1918989a5aa723660832e0e40e0f387be49
stlouisweb <stlouisweb@gmail.com>
2016-03-19 19:22:55 -0500

    tests

---
+++ tests/demo_test.js
@@ -0,0 +1,6 @@
+import assert from 'assert';
+describe('add', () => {
+  it('adds', () => {
+    assert.equal(1 + 1, 2);
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
-  style: path.join(__dirname, 'app/main.css')
+  style: path.join(__dirname, 'app/main.css'),
+  test: path.join(__dirname, 'tests')
 
 };
 
@@ -150,4 +151,31 @@ if(TARGET === 'build' || TARGET === 'stats') {
         })
       ]
     });
+}
+
+if(TARGET === 'test' || TARGET === 'tdd') {
+  module.exports = merge(common, {
+    devtool: 'inline-source-map',
+    resolve: {
+      alias: {
+        'app': PATHS.app
+      }
+    },
+    module: {
+      preLoaders: [
+        {
+          test: /\.jsx?$/,
+          loaders: ['isparta-instrumenter'],
+          include: PATHS.app
+        }
+      ],
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          loaders: ['babel?cacheDirectory'],
+          include: PATHS.test
+        }
+      ]
+    }
+  });
 }
